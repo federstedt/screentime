98015c0079be242d70a8db82dcc178e2874fef3d
Daniel Federstedt <daniel@braveops.se>
2024-12-11 20:54:09 +0100

    added some input and button functions for the timers

--- GUI/base_gui.gd
+++ GUI/base_gui.gd
@@ -1,10 +1,25 @@
 extends Control
 
+@onready var pause_button: Button = get_node("PauseButton")
+
 func _ready():
 	var main_node = get_parent()  # Main är föräldern till GUI
-	main_node.connect("timer_updated", Callable(self, "_on_timer_updated"))  # Koppla signalen
+	main_node.connect("timer_updated", Callable(self, "_on_timer_updated"))  # Koppla signalen för timern
+	main_node.connect("time_elapsed_updated", Callable(self, "_on_elapsed_updated"))
+	pause_button.connect("pressed", Callable(self, "_on_pause_pressed"))
 
 func _on_timer_updated(time_left: float):
 	var label: RichTextLabel = get_node("TimeLeftText")
 	var text = str(round(time_left)) + " seconds left"  # Uppdatera label
 	label.text = "[center]" + text + "[/center]"
+
+func _on_elapsed_updated(time:float):
+	var label: RichTextLabel = get_node("TimeElapsedText")
+	var text = "Elapsed: " + str(round(time))
+	label.text = "[center]" + text + "[/center]"
+	
+func _on_pause_pressed() -> void:
+	if pause_button.text == 'Pause':
+		pause_button.text = 'Unpause'
+	else:
+		pause_button.text = 'Pause'
--- main.gd
+++ main.gd
@@ -1,21 +1,45 @@
 extends Node
 
+# ref for timers: https://gamedevbeginner.com/how-to-make-a-timer-in-godot-count-up-down-in-minutes-seconds/
 signal timer_updated(time_left: float)
-
+signal time_elapsed_updated(time: float)
+#Exports
 @export var main_time:float = 10
+
+#Onready
 @onready var main_timer:Timer = get_node("MainTimer")  # Referens till din TimerNode
+@onready var reset_button:Button = get_node("BaseGUI/ResetButton")
+@onready var pause_button:Button = get_node("BaseGUI/PauseButton")
+@onready var time_elapsed := 0.0
 
 func _ready():
 	set_main_timer(main_time)
 	main_timer.start()  # Starta timern
 	main_timer.connect("timeout", Callable(self, "_on_timeout"))  # Koppla timeout-signalen
+	reset_button.connect("pressed", Callable(self, "_on_reset_pressed"))
+	pause_button.connect("pressed", Callable(self, "_on_pause_pressed"))
 
 func _process(delta: float):
+	time_elapsed += delta
+	emit_signal("time_elapsed_updated", time_elapsed)
 	emit_signal("timer_updated", main_timer.time_left)  # Skicka kontinuerligt tid kvar som signal
 
 func _on_timeout():
 	#print("Timer is done!")  # Exempel: Vad som händer när tiden är slut
-	pass
+	print("Timer ran out!")
 
 func set_main_timer(time: float):
 	main_timer.wait_time = time
+
+
+func _on_reset_pressed() -> void:
+	print('reset pressed')
+	main_timer.stop()
+	main_timer.start()
+
+func _on_pause_pressed() -> void:
+	print('pause pressed')
+	if main_timer.paused == true:
+		main_timer.paused = false
+	else:
+		main_timer.paused = true
