bd512fa6d626f4eccbbdc9a23b44ab249e317e1f
Daniel Federstedt <daniel@braveops.se>
2025-05-17 17:27:59 +0200

    basic api calls, and procs

--- backend/fasttime/routers/public.py
+++ backend/fasttime/routers/public.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+import psutil
 
 router = APIRouter()
 
@@ -6,3 +8,15 @@ router = APIRouter()
 @router.get("/")
 async def get_testroute():
     return "OK"
+
+
+@router.get("/processes")
+async def get_processes():
+    procs = []
+    for proc in psutil.process_iter(["pid", "name", "username"]):
+        print(proc.info)
+        procs.append(
+            {"pid": proc.pid, "name": proc.name(), "username": proc.username()}
+        )
+
+    return JSONResponse(content=procs)
--- backend_com.gd
+++ backend_com.gd
@@ -6,6 +6,8 @@ extends Node
 func _ready() -> void:
 	http_request.request_completed.connect(_on_http_req_completed)
 	get_backend_status()
+	#TODO: verkar som http request bara kan köras 1 gång eller varför körs inte nedan om ovan inte kommenteras bort ?
+	get_running_procs()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,6 +15,10 @@ func _process(delta: float) -> void:
 
 func get_backend_status() -> void:
 	var endpoint = "http://localhost:8000/v1/public/"
+	http_request.request(endpoint)
+
+func get_running_procs() -> void:
+	var endpoint = "http://localhost:8000/v1/public/processes"
 	http_request.request(endpoint)
 
 func _on_http_req_completed(result, response_code, headers, body) -> void:
